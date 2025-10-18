4d3a0f8e4e38352eff1cc88b28628cafd7188a44
Enos Andrade <enosads@gmail.com>
2025-10-17 23:45:09 -0300

    feat: Implement navigation between fragments
    This commit introduces navigation between `FirstFragment` and `SecondFragment` using the Navigation Component.
    
    Key changes include:
    *   Enabling View Binding in `build.gradle.kts`.
    *   Configuring `FragmentContainerView` as a `NavHostFragment` in `activity_main.xml`.
    *   Implementing click listeners in `MainActivity` to navigate between fragments and pass arguments.
    *   Updating `SecondFragment` to use View Binding and receive arguments.
    *   Defining a navigation action with arguments in `main_nav_graph.xml`.
    *   Adding new string resources for button text.

--- app/src/main/java/dev/enosads/fundamentosandroidapp/MainActivity.kt
+++ app/src/main/java/dev/enosads/fundamentosandroidapp/MainActivity.kt
@@ -3,22 +3,47 @@ package dev.enosads.fundamentosandroidapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
+import dev.enosads.fundamentosandroidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private  val navController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcvMainContainer) as? NavHostFragment
+        navHostFragment?.navController
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcvMainContainer) as NavHostFragment
-        val navController = navHostFragment.navController
+        binding.btnNextFragment.setOnClickListener {
+            navController?.currentDestination?.id.let {
+                when(it){
+                    R.id.firstFragment -> {
+                        navController?.navigate(R.id.action_firstFragment_to_secondFragment, bundleOf(
+                            "first_arg" to arrayOf("1", "2", "3")
+                        ))
+                        binding.btnNextFragment.text =
+                            getString(R.string.voltar_para_o_primeiro_fragment)
+                    }
+                    R.id.secondFragment -> {
+                        navController?.popBackStack()
+                        binding.btnNextFragment.text = getString(R.string.ir_para_proxima_tela)
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/dev/enosads/fundamentosandroidapp/SecondFragment.kt
+++ app/src/main/java/dev/enosads/fundamentosandroidapp/SecondFragment.kt
@@ -1,16 +1,36 @@
 package dev.enosads.fundamentosandroidapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dev.enosads.fundamentosandroidapp.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val firstArgument = arguments?.getStringArray("first_arg") ?: arrayOf()
+
+        Log.d("SecondFragment", "Argument: ${firstArgument.joinToString() }")
+
     }
 }
